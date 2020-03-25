341718da7278c6e4962f00ecfb48e9724fb44f8b
shh79 <shh2079@gmail.com>
2020-03-25 20:11:32 +0430

    create main window

---
+++ UI/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult r = MessageBox.Show("Are you sure ?", "Exit", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (r == MessageBoxResult.Yes)
+            {
+                Close();
+            }
+        }
+
+        private void tabcontrol_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void nextpg_Click(object sender, RoutedEventArgs e)
+        {
+            tabcontrol.SelectedItem = tab2;
+        }
+
+        private void prepg_Click(object sender, RoutedEventArgs e)
+        {
+            tabcontrol.SelectedItem = tab1;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
